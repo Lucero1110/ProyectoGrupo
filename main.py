4610fbd58c8cb68164eb5f0c74d92865b518e6ab
Lucero1110 <A01736938@tec.mx>
2025-04-06 23:42:46 -0600

    Código principal

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Hola a todos, este es un respositorio local y se desplegará en GITHUB')
+
+print('Este archivo se estara modificando por todos los miembros del equipo')
+
+print("se usaran funciones para sumar, restar, multiplicar y dificir dos números")
+
+
+a=10
+b=20
